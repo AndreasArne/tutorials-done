d85e209543e703e2fe5ffab2d68d937303395ca2
Andreas Arnesson <andreas_osby@hotmail.com>
2022-05-16 20:15:30 +0200

    new sveltekit tutorial about creating SSR blog using markdown

---
+++ sveltkit-markdown-blog/src/routes/__layout.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+  export const load = ({ url }) => {
+    const currentRoute = url.pathname
+
+    return {
+      props: {
+        currentRoute
+      }
+    }
+  }
+</script>
+
+<script>
+  import Header from '$lib/components/Header.svelte'
+  import '$lib/styles/style.scss'
+  import { fade } from 'svelte/transition'
+  import { prefetch, prefetchRoutes } from '$app/navigation'
+
+  prefetch('/blog') // Loads the blog page in the background
+  prefetchRoutes() // Loads ALL routes in the background. can be expensive if we habe many pages with much data
+
+export let currentRoute
+</script>
+
+<Header />
+
+{#key currentRoute}
+  <main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+    <slot />
+  </main>
+{/key}
+
+<footer>Hello, I'm the footer.</footer>
---
+++ sveltkit-markdown-blog/src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+  <title>About - Josh Collinsworth</title>
+</svelte:head>
+<h1>Hi, I'm Andreas!</h1>
+
+<p>This is my about page.</p>
