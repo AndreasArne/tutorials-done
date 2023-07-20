c433fb31d8ea0a79d661dfdad6d347ad33da6b21
Andreas Arnesson <andreas_osby@hotmail.com>
2023-07-20 14:48:41 +0200

    file upload i kungfoo

--- kungfoo/src/routes/+layout.svelte
+++ kungfoo/src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+    import { user, userData } from "$lib/firebase";
     import "../app.css";
+
+    // subscribe to stores globally
+    $user;
+    $userData;
 </script>
 
 <div class="min-h-screen flex flex-col">
