a97c8384b91adb0708685c7fa65ecc6ce575592a
AndreasArne <andreas_osby@hotmail.com>
2023-07-20 11:31:57 +0200

    added sveltekit kungfoo

---
+++ kungfoo/src/lib/firebase.ts
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAsAKigo9bcliPb0XBi5NUT1axmb6ViKDA",
+    authDomain: "kunfoo-41db8.firebaseapp.com",
+    projectId: "kunfoo-41db8",
+    storageBucket: "kunfoo-41db8.appspot.com",
+    messagingSenderId: "903101550157",
+    appId: "1:903101550157:web:4263703c9a8bf069e91604"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
+
+
+function userStore() {
+    let unsubscribe: () => void;
+    
+    if (!auth || !globalThis.window) {
+        console.warn("Auth is not initialized or not in browser");
+        const { subscribe} = writable<User | null>(null);
+        
+    }
+
+    const {subscribe} = writable(auth?.currentUser ?? null, (set) => {
+        unsubscribe = onAuthStateChanged(auth, (user) => {
+            set(user);
+        });
+        return () => unsubscribe();
+    });
+
+    return {
+        subscribe, 
+    };
+}
+
+export const user = userStore();
---
+++ kungfoo/src/routes/login/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import {page} from "$app/stores";
+    import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
+</script>
+
+
+<nav class="flex justify-center my-6">
+    <ul class="steps">
+        <a href="/login" class="step step-primary">Sign in</a>
+        <!--
+            https://svelte.dev/tutorial/classes
+            https://learn.svelte.dev/tutorial/page-store
+         -->
+        <a href="/login/username"
+            class="step"
+            class:step-primary={$page.route.id?.match(/username|photo/g)}> 
+            Choose username
+        </a>
+        <a 
+            href="/login/photo" 
+            class="step"
+            class:step-primary={$page.route.id?.match(/photo/g)}>
+            Upload Photo
+        </a>
+    </ul>
+</nav>
+
+<AnimatedRoute>
+<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+    <div class="card-body items-center text-center">
+        <slot></slot>
+
+    </div>
+</main>
+</AnimatedRoute>
